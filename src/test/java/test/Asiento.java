4b9eaee1c960cd1d6b43d4795a1e3f17b6d285a3
juandresh <141792824+juandresh@users.noreply.github.com>
2023-08-19 14:54:40 -0500

    Clase Asiento

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -7,15 +7,7 @@ public class Asiento {
 	
 	void cambiarColor(String color)
 	{
-		if(color =="rojo")
-			this.color = color;		
-		else if (color == "verde")
-			this.color = color;	
-		else if (color == "amarillo")
-			this.color = color;	
-		else if (color == "negro")
-			this.color = color;	
-		else if (color == "blanco")
+		if(color =="rojo" || color == "verde" || color == "amarillo" || color == "negro" || color == "blanco")
 			this.color = color;
 	}
 }
