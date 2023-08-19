e10a13de6404a1f449ba0bff14c6155ecb9635aa
juandresh <141792824+juandresh@users.noreply.github.com>
2023-08-19 14:48:12 -0500

    Clase Auto

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,5 +1,33 @@
 package test;
 
 public class Auto {
-
+	String modelo;
+	int precio;
+	Asiento[] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	int cantidadCreados;
+	
+	int cantidadAsientos()
+	{
+		int cantAsientos=0;
+		for(int i=0; i<asientos.length;i++)
+			if(asientos[i] != null)
+				cantAsientos++;
+		return cantAsientos;
+	}
+	
+	String verificarIntegridad()
+	{
+		if(registro == motor.registro) { 
+			for(int i = 0; i<asientos.length; i++)
+				if(asientos[1] != null)
+					if(asientos[i].registro != registro)
+						return("Las piezas no son originales");
+			return"Auto original";}
+		else 
+			return"Las piezas no son originales";
+		
+	}
 }
